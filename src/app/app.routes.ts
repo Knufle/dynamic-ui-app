62d616a5ecffab90629b8346748d0d71da55da92
Knufle <iamknufle@gmail.com>
2025-03-30 11:53:44 -0300

    feat: add routing for DynamicPageComponent with default redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DynamicPageComponent } from './components/dynamic-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'page/:path', component: DynamicPageComponent },
+    { path: '', redirectTo: 'page/home', pathMatch: 'full' }
+];
