0caeb1e28bc7c92dcdfffa64f7ebe778a9fee627
Knufle <iamknufle@gmail.com>
2025-03-30 12:13:47 -0300

    feat: remove NavigationComponent from AppComponent template and imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavigationComponent } from './components/navigation.component';
 
 @Component({
     selector: 'app-root',
-    template: `
-        <app-navigation></app-navigation>
-        <router-outlet></router-outlet>
-    `,
+    template: `<router-outlet></router-outlet>`,
     standalone: true,
-    imports: [RouterOutlet, NavigationComponent]
+    imports: [RouterOutlet]
 })
 export class AppComponent {
     title = 'dynamic-ui-app';
