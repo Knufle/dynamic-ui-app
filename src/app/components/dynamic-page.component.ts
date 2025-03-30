27c3378917573c10e78b7f8d1c21c28924e90c7c
Knufle <iamknufle@gmail.com>
2025-03-30 11:51:12 -0300

    feat: add DynamicPageComponent for rendering dynamic pages based on route parameters

---
+++ src/app/components/dynamic-page.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { DynamicRendererComponent } from '../dynamic-renderer.component';
+import { DynamicPage, DynamicUIData } from '../interfaces/dynamic-ui.interface';
+
+@Component({
+    selector: 'app-dynamic-page',
+    standalone: true,
+    imports: [CommonModule, DynamicRendererComponent],
+    template: `
+        <app-dynamic-renderer *ngIf="currentPage" [sections]="currentPage.sections"></app-dynamic-renderer>
+    `
+})
+export class DynamicPageComponent implements OnInit {
+    currentPage?: DynamicPage;
+
+    constructor(
+        private route: ActivatedRoute,
+        private http: HttpClient
+    ) {}
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            const path = params['path'];
+            this.loadPageData(path);
+        });
+    }
+
+    private loadPageData(path: string) {
+        this.http.get<DynamicUIData>('http://localhost:3000/ui-data')
+            .subscribe(data => {
+                this.currentPage = data.pages.find(page => page.path === path);
+            });
+    }
+}
