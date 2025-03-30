cfd471f7cf7eafbfb4fe6d9399383805a42f5f67
Knufle <iamknufle@gmail.com>
2025-03-30 11:53:52 -0300

    feat: update main.ts to bootstrap AppComponent and include HttpClientModule

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,13 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication, BrowserModule } from '@angular/platform-browser';
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
-import { DynamicRendererComponent } from './app/dynamic-renderer.component';
-import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
-bootstrapApplication(DynamicRendererComponent, {
+bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule),
+        importProvidersFrom(BrowserModule, HttpClientModule),
         provideRouter(routes)
     ]
-})
-  .catch(err => console.error(err));
+}).catch(err => console.error(err));
